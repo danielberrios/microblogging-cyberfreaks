e2dfdc673423cc9cbc2584a1a68776e96908918a
hectordael371 <hector.jimenez@upr.edu>
2021-04-12 12:02:05 -0400

    Successfully integrated and tested like interaction.

--- server-side/controllers/post.controller.js
+++ server-side/controllers/post.controller.js
@@ -27,51 +27,6 @@ class PostController {
         });
     }
     
-    likePost(req, res) {
-        const likeData = req.body;
-
-        PostModel.insertNewLike(likeData).then(() => {
-            res.status(201).send("Successfully published new like.")
-        })
-        .catch((error) => {
-            console.log("Error: ", error)
-            res.status(409).send(error.message)
-        });
-    }
-    
-    removeLike(req, res) {
-        const [uid, post_id] = [req.body.uid, req.query.post_id]
-
-        PostModel.handleUnlike(uid, post_id)
-        .then(() => {
-            res.status(201).send("Successfully removed like from the post.")
-        })
-        .catch((error) => {
-            if(error.message == "Post ID does not exist.") //Falta el otro message del handler
-                res.status(404).send(error.message)
-            else
-                res.status(500).send("An error has ocurred.")
-        })
-    }
-
-    getUsersThatLikedPost(req, res) {
-        //Not sure about this, still needs verification
-        const post_id = req.params.pid   
-
-        console.log("PostId: ", post_id)
-
-        PostModel.getUsersThatLikedAPost(post_id)
-            .then((user) => {
-                if(user)
-                    res.status(200).json(user)
-                else
-                    res.status(404).send("User not found.")
-            })
-            .catch((error) => {
-                res.status(500).send("An error has occurred.")
-            })
-    }
-
     publishShare(req, res) {
         const shareData = req.body;
 
@@ -85,9 +40,7 @@ class PostController {
     }
 
     getPostById(req, res) {
-        const postId = req.params.pid   
-
-        console.log("PostId: ", postId)
+        const postId = req.query.post_id   
 
         PostModel.getSpecificPostWith(postId)
             .then((post) => {
@@ -113,7 +66,63 @@ class PostController {
         });
     }
 
-    unlik
+    likePost(req, res) {
+        const [post_id, uid] = [req.query.post_id, req.body.uid]
+
+        PostModel.insertNewLike(post_id, uid).then(() => {
+            res.status(201).send("Successfully published new like.")
+        })
+        .catch((error) => {
+            switch (error.constraint) {
+                case 'likes_pkey':
+                    res.status(409).send("Like already exists.")
+                    break;
+
+                case 'posts_uid_fkey':
+                    res.status(404).send("User doesn't exist.")
+                    break;
+
+                case 'likes_post_id_fkey':
+                    res.status(404).send("Post doesn't exist.")
+                    break;
+
+                default:
+                    res.status(500).send("Some error has occurred.")
+                    break;
+            }
+        });
+    }
+    
+    removeLike(req, res) {
+        const [uid, post_id] = [req.body.uid, req.query.post_id]
+
+        PostModel.handleUnlike(uid, post_id)
+        .then(() => {
+            res.status(201).send("Successfully removed like from the post.")
+        })
+        .catch((error) => {
+            if(error.message == "Post ID does not exist.")
+                res.status(404).send(error.message)
+            else
+                res.status(500).send("Some error has occurred.")
+        })
+    }
+
+    getUsersThatLikedPost(req, res) {
+        const post_id = req.query.post_id  
+
+        PostModel.getUsersThatLikedAPost(post_id)
+            .then((users) => {
+                // If users is empty, then no users has liked the post.
+                res.status(201).send(users)
+            })
+            .catch((error) => {
+                if(error.message == "Post ID does not exist.")
+                    res.status(404).send(error.message)
+                else
+                    res.status(500).send("Some error has occurred.")
+            })
+    }
 }
 
 module.exports = new PostController()
